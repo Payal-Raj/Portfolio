710c14064ca9eac8e6e7567a90cef1e4fff1c58a
Payal-Raj <piyushikumari35@gmail.com>
2026-01-17 22:09:05 +0500

    Header Updated

---
+++ script.js
@@ -0,0 +1,38 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+let sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
+
+function myFunction() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
